5f85a32da58304203aa280538ccb24f52c42f693
AHM-Moinuddin-Sami <moinuddinsami1205@gmail.com>
2023-06-20 14:58:59 +0600

    completed investors section

--- src/Components/Investors/Investors.jsx
+++ src/Components/Investors/Investors.jsx
@@ -1,60 +1,108 @@
 "use client";
 import Image from "next/image";
+import { useEffect } from "react";
 import Tilt from 'react-parallax-tilt';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Investors = () => {
+
+    useEffect(() => {
+        AOS.init({
+            // Customize AOS options here (optional)
+        });
+    }, [])
+
+    const companies = [
+        "/InvCompany1.webp",
+        "/InvCompany2.webp",
+        "/InvCompany3.webp",
+        "/InvCompany4.webp",
+    ]
+
+    const investors = [
+        {
+            name: "Kunal Shah",
+            title: "Founder, CRED",
+            img: "/Inv1.webp"
+        },
+        {
+            name: "Gaurav Munjal",
+            title: "Founder, UNACADEMY",
+            img: "/Inv2.webp"
+        },
+        {
+            name: "Aakrit Vaish",
+            title: "Co-Founder, HAPTIK",
+            img: "/Inv3.webp"
+        },
+        {
+            name: "Harshil Mathur",
+            title: "Co-Founder, RAZORPAY",
+            img: "/Inv4.webp"
+        },
+        {
+            name: "Vidit Aatrey",
+            title: "Founder & CEO, MEESHO",
+            img: "/Inv5.webp"
+        },
+        {
+            name: "Amirsh Rau",
+            title: "CEO, PINELABS",
+            img: "/Inv6.webp"
+        },
+        {
+            name: "Lalit Keshre",
+            title: "CEO, GROWW",
+            img: "/Inv7.webp"
+        },
+        {
+            name: "Gokul Rajaram",
+            title: "Product, DOORDASH",
+            img: "/Inv8.webp"
+        },
+    ]
+
     return (
         <div className="px-48 pt-[2vw] pb-[8vw]">
             <p className="my-12 text-[40px] font-extrabold">Meet the Investors</p>
             <div className="flex gap-[2vw] mt-[4vw] justify-between">
-                <Tilt
-                    tiltMaxAngleX={10}
-                    tiltMaxAngleY={10}
-                >
-                    <Image
-                        alt=""
-                        height={220}
-                        width={350}
-                        src={"/InvCompany1.webp"}
-                        className="p-8 duration-[400ms] hover:transition-all hover:duration-[400ms] hover:scale-[1.03]"
-                    ></Image>
-                </Tilt>
-                <Tilt
-                    tiltMaxAngleX={10}
-                    tiltMaxAngleY={10}
-                >
-                    <Image
-                        alt=""
-                        height={220}
-                        width={350}
-                        src={"/InvCompany2.webp"}
-                        className="p-8 duration-[400ms] hover:transition-all hover:duration-[400ms] hover:scale-[1.03]"
-                    ></Image>
-                </Tilt>
-                <Tilt
-                    tiltMaxAngleX={10}
-                    tiltMaxAngleY={10}
-                >
-                    <Image
-                        alt=""
-                        height={220}
-                        width={350}
-                        src={"/InvCompany3.webp"}
-                        className="p-8 duration-[400ms] hover:transition-all hover:duration-[400ms] hover:scale-[1.03]"
-                    ></Image>
-                </Tilt>
-                <Tilt
-                    tiltMaxAngleX={10}
-                    tiltMaxAngleY={10}
-                >
-                    <Image
-                        alt=""
-                        height={220}
-                        width={350}
-                        src={"/InvCompany4.webp"}
-                        className="p-8 duration-[400ms] hover:transition-all hover:duration-[400ms] hover:scale-[1.03]"
-                    ></Image>
-                </Tilt>
+                {
+                    companies.map((comapny, index) => <Tilt
+                        key={index}
+                        tiltMaxAngleX={10}
+                        tiltMaxAngleY={10}
+                    >
+                        <Image
+                            data-aos='fade-up'
+                            alt=""
+                            height={220}
+                            width={350}
+                            src={comapny}
+                            className="p-8 duration-[200ms] hover:transition-all hover:duration-[200ms] hover:scale-[1.03]"
+                        ></Image>
+                    </Tilt>)
+                }
+            </div>
+            <div className="mt-[4vw] grid justify-between md:grid-cols-5 gap-[2vw]">
+                {
+                    investors.map((investor, index) => <Tilt
+                        key={index}
+                        tiltMaxAngleX={5}
+                        tiltMaxAngleY={5}
+                    >
+                        <div className="p-8 duration-[200ms] hover:transition-all hover:duration-[200ms] hover:scale-[1.03]">
+                            <Image
+                                alt=""
+                                height={340}
+                                width={400}
+                                src={investor.img}
+                            ></Image>
+                            <p className="mt-4 font-medium">{investor.name}</p>
+                            <p>{investor.title}</p>
+                        </div>
+                    </Tilt>)
+                }
             </div>
         </div>
     );
